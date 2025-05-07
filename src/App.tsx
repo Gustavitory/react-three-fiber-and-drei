8496ff0fec80cdfecb02665f18a980d4b8a2e0f0
Gustavo Riera <ggrieraya@gmail.com>
2025-05-07 01:14:34 -0400

    improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import { OrbitControls, Sparkles, Text } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { type Mesh } from "three";
-import { useRef, useState } from "react";
+import { useRef, useState, memo } from "react";
+
+const MemoizedSparkles = memo(() => (
+  <Sparkles
+    speed={0.1}
+    color={"orange"}
+    size={6}
+    scale={2}
+    count={20}
+    noise={0.2}
+  />
+));
+
+type rotatingCubeProps = {
+  isRotating: boolean;
+};
 
 //Componente que renderiza un cubo que gira y se detiene cuando el mouse está sobre él
-const RotatingCube = () => {
+const RotatingCube = ({ isRotating }: rotatingCubeProps) => {
   const meshRef = useRef<Mesh>(null); //Referencia al cubo
-  const [isRotating, setIsRotating] = useState<boolean>(true); //Estado para controlar si el cubo está girando
   const defaultRotationSpeed = 0.01; //Velocidad de rotación por defecto
   const acceleration = 0.00005; //Aceleración de la rotación
   const [rotationSpeed, setRotationSpeed] =
@@ -31,49 +45,62 @@ const RotatingCube = () => {
     <>
       {/* Si el cubo no está girando, muestra el texto "Stopping" o "Stopped" dependiendo de la velocidad de rotación */}
       {!isRotating && (
-        <Text position={[0, 0, 2]} color={"#000000"} fontSize={0.4}>
+        <Text
+          position={[0, 0, 2]}
+          color={"#000000"}
+          fontSize={0.4}
+          anchorX={"center"}
+          anchorY={"middle"}
+        >
           {rotationSpeed > 0 ? "Stopping" : "Stopped"}
         </Text>
       )}
-      <mesh
-        ref={meshRef}
-        onPointerEnter={() => setIsRotating(false)}
-        onPointerLeave={() => setIsRotating(true)}
-      >
+      <mesh ref={meshRef}>
         {/* Cubo */}
         <boxGeometry args={[1.5, 1.5, 1.5]} />
         {/* Material del cubo */}
         <meshLambertMaterial color={"#468585"} emissive={"#468585"} />
         {/* Efecto de estrellas */}
-        <Sparkles
-          speed={0.001}
-          color={"orange"}
-          size={6}
-          scale={2}
-          count={20}
-          noise={0.2}
-        />
+        <MemoizedSparkles />
       </mesh>
     </>
   );
 };
 
 const App = () => {
+  const [isRotating, setIsRotating] = useState(true);
   return (
-    <Canvas
-      style={{
-        height: "100vh",
-        width: "100vw",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <OrbitControls enableZoom={true} enablePan enableRotate />
-      <directionalLight position={[1, 1, 1]} intensity={10} color={0x9cdba6} />
-      <color attach={"background"} args={["#F0F0F0"]} />
-      <RotatingCube />
-    </Canvas>
+    <div style={{ position: "relative" }}>
+      <button
+        onClick={() => setIsRotating(!isRotating)}
+        style={{
+          position: "absolute",
+          top: "40px",
+          left: "40px",
+          zIndex: "100",
+        }}
+      >
+        {isRotating ? "stop rotation" : "start rotation"}
+      </button>
+      <Canvas
+        style={{
+          height: "100vh",
+          width: "100vw",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <OrbitControls enableZoom={true} enablePan enableRotate />
+        <directionalLight
+          position={[1, 1, 1]}
+          intensity={10}
+          color={0x9cdba6}
+        />
+        <color attach={"background"} args={["#F0F0F0"]} />
+        <RotatingCube isRotating={isRotating} />
+      </Canvas>
+    </div>
   );
 };
 
